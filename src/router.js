ae86c5f1eb18172b3c9b16c89e17162974e51b92
emilyliu0206 <55123024+emilyliu0206@users.noreply.github.com>
2019-11-18 22:36:17 -0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,12 +5,16 @@ import Login from './Login/Login.jsx';
 import Register from './Register/Register.jsx';
 import Home from './Home/Home.jsx';
 import Error from './Error/Error.jsx';
+import ItemDetailPage from './itemDetailPage/ItemDetailPage.js';
+import ResultList from './searchResult/ResultList.js';
 
 const BasicRoute = () => (
     <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/Item-Detail-Page' component={ItemDetailPage} />
+        <Route exact path='/Search-Result' component={ResultList} />
         <Route component={Error} />
     </Switch>
 );
