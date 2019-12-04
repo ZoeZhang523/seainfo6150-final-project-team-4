c6a957bdb9e45f92e70403e47cecf225df05b1d8
“ZoeZhang523” <839921303@qq.com>
2019-12-03 18:52:26 -0800

    Add antd

--- src/PublicComponents/Footer/Footer.jsx
+++ src/PublicComponents/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './Footer.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Login from '../../Login/Login';
+import { Icon} from 'antd';
+import 'antd/dist/antd.css';
+import Category from '../../Category/Category.jsx';
 
 class Footer extends Component {
     render() {
@@ -9,62 +11,80 @@ class Footer extends Component {
             <Router>
                 <div className='footer'>
                     <div className='customerService'>
-                        <h3>Customer Service</h3>
+                        <h2>Customer Service</h2>
                         <div className='customerServiceList'>
                             <ul className='animals'>
                                 <li>
-                                    <Link to='/OrderPage'>Track Order</Link>
+                                    <h3><Link to='/AboutUs'>About Us</Link></h3>
                                 </li>
                                 <li>
-                                    <Link to='/help'>Help</Link>
+                                    <p>Contact Us:</p>
                                 </li>
                                 <li>
-                                    <Link to='/contact'>Contact Us</Link>
+                                    <Icon className="contact" type="facebook" style={{fontSize: '25px'}}/>
+                                    <Icon className="contact" type="twitter" style={{fontSize: '25px'}}/>
+                                    <Icon className="contact" type="instagram" style={{fontSize: '25px'}}/>
                                 </li>
                             </ul>
-                        </div>
-                    </div>
-                    <div className='about'>
-                        <h3>About Us</h3>
-                        <div className='aboutContent'>
-                            <p>12345</p>
                         </div>
                     </div>
                     <div className='siteMap'>
-                        <h3>Site Map</h3>
+                        <h2>Site Map</h2>
                         <div className='siteMapCat'>
                             <ul className='catList'>
                                 <li>
-                                    <Link to='/cat'>Cat</Link>
+                                    <h3><Link to='/category/cat'>Cat</Link></h3>
                                 </li>
                                 <li>
-                                    <Link to='/cat/food'>Cat Food</Link>
+                                    <Link to='/category/cat/food/dry_food'>Dry Food</Link>
                                 </li>
                                 <li>
-                                    <Link to='/cat/toys'>Cat Toys</Link>
+                                    <Link to='/category/cat/food/wet_food'>Wet Food</Link>
                                 </li>
                                 <li>
-                                    <Link to='/cat/beds'>Cat Beds</Link>
+                                    <Link to='/category/cat/toy'>Cat Toys</Link>
+                                </li>
+                                <li>
+                                    <Link to='/category/cat/bed'>Cat Beds</Link>
                                 </li>
                             </ul>
+                        </div>
                         </div>
                         <div className='siteMapDog'>
                             <ul className='dogList'>
                                 <li>
-                                    <Link to='/dog'>Dog</Link>
+                                    <h3><Link to='/category/dog'>Dog</Link></h3>
                                 </li>
                                 <li>
-                                    <Link to='/dog/food'>Dog Food</Link>
+                                    <Link to='/category/dog/food/dry_food'>Dry Food</Link>
                                 </li>
                                 <li>
-                                    <Link to='/dog/toys'>Dog Toys</Link>
+                                    <Link to='/category/dog/food/wet_food'>Wet Food</Link>
                                 </li>
                                 <li>
-                                    <Link to='/dog/beds'>Dog Beds</Link>
+                                    <Link to='/category/dog/toy'>Dog Toys</Link>
+                                </li>
+                                <li>
+                                    <Link to='/category/dog/bed'>Dog Beds</Link>
                                 </li>
                             </ul>
                         </div>
-                    </div>
+                        <div className='siteMapSmallPet'>
+                            <ul className='dogList'>
+                                <li>
+                                    <h3><Link to='/category/small_pet'>Small Pet</Link></h3>
+                                </li>
+                                <li>
+                                    <Link to='/category/small_pet/food'>Small Pet Food</Link>
+                                </li>
+                                <li>
+                                    <Link to='/category/small_pet/toy'>Small Pet Toys</Link>
+                                </li>
+                                <li>
+                                    <Link to='/category/small_pet/bed'>Small Pet Beds</Link>
+                                </li>
+                            </ul>
+                        </div>
 
                     <div className='cute'>
                         <img
@@ -74,15 +94,26 @@ class Footer extends Component {
                     </div>
 
                     <Switch>
-                        <Route path='/cat' exact component={Login} />
-                        <Route path='/cat/food' exact component={Login} />
-                        <Route path='/cat/toys' exact component={Login} />
-                        <Route path='/cat/beds' exact component={Login} />
+                        <Route path='/aboutUs' exact component={Category} />
 
-                        <Route path='/dog' exact component={Login} />
-                        <Route path='/dog/food' exact component={Login} />
-                        <Route path='/dog/toys' exact component={Login} />
-                        <Route path='/dog/beds' exact component={Login} />
+                        <Route path='/category/cat' exact component={Category} />
+                        <Route path='/category/cat/food' exact component={Category} />
+                        <Route path='/category/cat/food/dry_food' exact component={Category} />
+                        <Route path='/category/cat/food/wet_food' exact component={Category} />
+                        <Route path='/category/cat/toy' exact component={Category} />
+                        <Route path='/category/cat/bed' exact component={Category} />
+
+                        <Route path='/category/dog' exact component={Category} />
+                        <Route path='/category/dog/food' exact component={Category} />
+                        <Route path='/category/dog/food/dry_food' exact component={Category} />
+                        <Route path='/category/dog/food/wet_food' exact component={Category} />
+                        <Route path='/category/dog/toy' exact component={Category} />
+                        <Route path='/category/dog/bed' exact component={Category} />
+                        
+                        <Route path='/category/small_pet' exact component={Category} />
+                        <Route path='/category/small_pet/food' exact component={Category} />
+                        <Route path='/category/small_pet/toy' exact component={Category} />
+                        <Route path='/category/small_pet/bed' exact component={Category} />
                     </Switch>
                 </div>
             </Router>
